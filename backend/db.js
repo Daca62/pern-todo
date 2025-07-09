cb353d680e15cbcab8e884001e539fb3111138c6
Daca62 <167454793+Daca62@users.noreply.github.com>
2025-07-09 19:27:52 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "daca1999",
+  password: "daca1111",
   host: "localhost",
   port: 5432,
   database: "pernblog",
